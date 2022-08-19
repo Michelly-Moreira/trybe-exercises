6757948b61967b95a79bed9cced87e82a32e9d44
Michelly <michelly.daiana@hotmail.com>
2022-08-19 02:08:42 -0300

    inicio dos exercicios js

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercise.js
@@ -0,0 +1,39 @@
+1
+const a = 13;
+const b = 13;
+
+let adicao = a+b;
+console.log(adicao);
+
+let subtracao = a-b;
+console.log(subtracao);
+
+let multiplicacao = a*b;
+console.log(multiplicacao);
+
+let divisao = a/b;
+console.log(divisao);
+
+let modulo = a%b;
+console.log(modulo);
+
+2.
+const c = 10;
+const d = 3;
+let resultado;
+
+if(c>d){
+    console.log(resultado = c);
+}
+else if(c<d){
+    console.log(resultado = d);
+}
+else{
+    console.log(resultado = 'ambos são iguais');
+}
+
+3.
+
+
+
+
