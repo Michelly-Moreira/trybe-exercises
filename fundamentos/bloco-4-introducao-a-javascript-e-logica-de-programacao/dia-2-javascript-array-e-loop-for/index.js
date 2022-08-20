81994b8c442318338d05ae17c6d4fbd86bd7344c
Michelly <michelly.daiana@hotmail.com>
2022-08-19 21:21:17 -0300

    realização do exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -25,5 +25,23 @@ for(i=0;i<numbers.length;i+=1){
     console.log(resultado);
     }
 }
-*/
+
 4.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for(i=0;i<numbers.length;i+=1){
+    resultado = (numbers[i]+resultado);
+
+    if(i==numbers.length-1){
+    resultado = resultado/numbers.length;
+    
+    if (resultado>20){
+        console.log("valor maior que 20");
+    }else{
+        console.log("valor menor que 20");
+    }
+}
+}
+*/
+5.
