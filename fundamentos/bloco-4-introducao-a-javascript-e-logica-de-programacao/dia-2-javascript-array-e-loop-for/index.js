10bd21b3d5dbd4d23d0c4a31c75484881153876e
Michelly <michelly.daiana@hotmail.com>
2022-08-19 21:07:24 -0300

    atualizando até exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,5 +1,29 @@
+/*1.
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(i=0;i<numbers.length;i+=1){
     console.log(numbers[i])
 }
+
+2.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for(i=0;i<numbers.length;i+=1){
+    resultado = (numbers[i]+resultado)
+    console.log(resultado)
+}
+ 
+3.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for(i=0;i<numbers.length;i+=1){
+    resultado = (numbers[i]+resultado);
+    if(i==numbers.length-1){
+    resultado = resultado/numbers.length;
+    console.log(resultado);
+    }
+}
+*/
+4.
