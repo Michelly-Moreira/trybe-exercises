ff4339d568d2201ea755d0aa72dfacdf0769ff51
Michelly <michelly.daiana@hotmail.com>
2022-08-20 16:51:05 -0300

    restante dos exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,9 +1,7 @@
 /*1.
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(i=0;i<numbers.length;i+=1){
-    console.log(numbers[i])
-}
+console.log(numbers)
 
 2.
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
@@ -42,6 +40,56 @@ for(i=0;i<numbers.length;i+=1){
         console.log("valor menor que 20");
     }
 }
+
 }
 */
-5.
+5. ------------ERRADO!!!
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let firstnumber;
+
+for(i=1;i<numbers.length;i+=1){
+   numbers[i]
+    if(numbers[0]>numbers[i]){
+        firstnumber=numbers[0];
+    } else {firstnumber=numbers[i];
+    }
+    console.log(firstnumber)
+}
+
+6.   --------ERRADO!!!
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (i=0;i<numbers.length;i+=1){
+    if (numbers[i] %2 != 0){
+        console.log(numbers[i]);
+    }else{
+        console.log("nenhum valor ímpar encontrado");
+    }
+}
+
+7.
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let firstnumber;
+
+for(i=1;i<numbers.length;i+=1){
+   numbers[i]
+    if(numbers[0]<numbers[i]){
+        firstnumber=numbers[0];
+    } else {firstnumber=numbers[i];
+    }
+    console.log(firstnumber)
+}
+/*
+8.
+let meuArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
+for(i = 0;i<meuArray.length;i+=1){
+    console.log(meuArray[i]);
+}
+
+9.
+let meuArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+for(i = 0;i<meuArray.length;i+=1){
+     console.log(meuArray[i]/2);
+}
+*/
