967bed0ba73fe380a260b31ca32108ee5e6f1b11
Michelly <michelly.daiana@hotmail.com>
2022-10-08 17:18:16 -0300

    describe + before e afterEach

--- front-end/secao-1-javascript-e-testes-assincronos/dia-1-js-assincronos-e-callbacks/setupAndTeardown/cicles.test.js
+++ front-end/secao-1-javascript-e-testes-assincronos/dia-1-js-assincronos-e-callbacks/setupAndTeardown/cicles.test.js
@@ -1,5 +1,5 @@
 // cicles.test.js
-
+/* 
 let cities = [];
 
 const addCity = (city) => {
@@ -24,11 +24,11 @@ beforeEach(() => {
 afterEach(() => {
   cities = [];
   //return cities;
-}); 
+});  */
 // afterEach é executada após cada teste. Para evitar que o que é feito em um teste “vaze” para o outro. Podendo limpar nosso array ao final de cada teste usando o afterEach. Etapa: Teardown
  
 
-//Etapa de teste, realizada abaixo:
+//Etapa de testes, realizada abaixo:
 /* test('Testa a função addCity', () => {
   expect.assertions(4);
   addCity('Campinas');
@@ -91,4 +91,77 @@ test('Testa a função removeCity utilizando o afterEach', () => {
   removeCity('Pindamonhangaba');
   expect(cities).toHaveLength(0);
   expect(cities).not.toContain('Pindamonhangaba');
+}); */
+
+
+/* Utilizando BEFOREEACH e AFTEREACH junto com DESCRIBE
+Tenho um bloco de describe agrupando os testes e o beforeEach ou afterEach estão dentro desse describe, ele rodará apenas dentro dos testes que estão nesse describe, ou seja, como há o segundo describe, aquele beforeEach e afterEach que estão no primeiro describe não serão rodados antes e/ou depois dos testes do segundo.
+// cicles.test.js
+ let cities = [];
+
+ const addCity = (city) => {
+ cities.push(city);
+ };
+
+ const removeCity = (city) => {
+ cities = cities.filter((eachCity) => eachCity !== city);
+ };
+
+describe('Agrupa o primeiro bloco de testes', () => {
+  beforeEach(() => {
+    cities = [...cities, 'Pindamonhangaba'];
+  });
+
+  afterEach(() => {
+    cities = [];
+  });
+
+  test('Testa a função addCity dentro do primeiro bloco de testes', () => {
+    expect.assertions(5);
+    addCity('Campinas');
+    addCity('Goiania');
+    addCity('Recife');
+    expect(cities).toHaveLength(4);
+    expect(cities).toContain('Campinas');
+    expect(cities).toContain('Goiania');
+    expect(cities).toContain('Recife');
+    expect(cities).toContain('Pindamonhangaba');
+  });
+
+  test('Testa a função removeCity dentro do primeiro bloco de testes', () => {
+    expect.assertions(2);
+    removeCity('Pindamonhangaba');
+    expect(cities).toHaveLength(0);
+    expect(cities).not.toContain('Pindamonhangaba');
+  });
+});
+
+describe('Agrupa o segundo bloco de testes', () => {
+  beforeEach(() => {
+    cities = [...cities, 'Tangamandapio'];
+  });
+
+  afterEach(() => {
+    cities = [];
+  });
+
+  test('Testa a função addCity dentro do segundo bloco de testes', () => {
+    expect.assertions(5);
+    addCity('Campinas');
+    addCity('Goiania');
+    addCity('Recife');
+    expect(cities).toHaveLength(4);
+    expect(cities).toContain('Campinas');
+    expect(cities).toContain('Goiania');
+    expect(cities).toContain('Recife');
+    expect(cities).toContain('Tangamandapio');
+  });
+
+  test('Testa a função removeCity dentro do segundo bloco de testes', () => {
+    expect.assertions(3);
+    removeCity('Tangamandapio');
+    expect(cities).toHaveLength(0);
+    expect(cities).not.toContain('Tangamandapio');
+    expect(cities).not.toContain('Pindamonhangaba');
+  });
 }); */
